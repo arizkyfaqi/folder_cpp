a9f780a72359a49f38ee0e8b945d6d20534b34db
Ade Rizky <aderizkyfaqi@gmail.com>
2019-07-18 23:45:52 +0700

    ada konflik

--- tipe_data.cpp
+++ tipe_data.cpp
@@ -13,8 +13,8 @@ int main()
 
 	cout << a << endl;
 	cout << sizeof(a) << " byte" << endl;
-	cout << "nilai maksimal Integer : " << numeric_limits<int>::max() << endl;
-	cout << "nilai minimal Integer : " << numeric_limits<int>::min() << endl;
+	cout << numeric_limits<int>::max() << endl;
+	cout << numeric_limits<int>::min() << endl;
 	cin.get();
 	return 0;
 }
