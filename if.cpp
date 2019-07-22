c526a8c925fad53ef118c6941cd5b0f93818ff8b
arfa <arfa@example.com>
2019-07-22 20:03:24 +0700

    if statement

---
+++ if.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cout << "Masukan angka = ";
+	cin >> a;
+
+	//if statement
+	//kondisi dalam bentuk boolean
+	if (a == 3)
+	{
+		cout << "Welcome to The Jungle" << endl;
+	}
+
+	cout << "Selesai" << endl;
+	cin.get();
+	return 0;
+}
