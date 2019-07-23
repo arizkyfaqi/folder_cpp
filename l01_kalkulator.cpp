22ffac2d3e9f0cc8e7f956c3f34ffc5c78ee7e9b
arfa <arfa@example.com>
2019-07-23 17:21:55 +0700

    kalkulator

---
+++ l01_kalkulator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+ using namespace std;
+
+int main()
+{
+	float hasil, a, b;
+	char opr;
+
+	cout << "Selamat datang di Program Kalkulator" << endl;
+	cout << "masukan nilai a : ";
+	cin >> a;
+	cout << "Masukan operator : " << endl;
+
+
+	return 0;
+}
