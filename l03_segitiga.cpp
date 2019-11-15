5c7e5e9f6fc0d31cde111b949f894dfd79386eb5
arfa <arfa@example.com>
2019-11-15 11:38:55 +0700

    latihan segitiga pola 3

--- l03_segitiga.cpp
+++ l03_segitiga.cpp
@@ -17,15 +17,36 @@ int main()
 		cout << endl;
 	}
 
+	cout << "\n";
+
 	cout << "Pola 2 \n";
 	for (int i = 1; i <= n; ++i){
-		cout << i;
-		for (int j = n; j <= i; ++j){
-			cout << j;
+		// cout << i;
+		for (int j = n; j >= i; --j){
+			// cout << j;
 			cout << "*";
 		}
 		cout << endl;
 	}
+
+	cout << "\n";
+
+	cout << "Pola 3 \n";
+	for (int i = 1; i <= n; ++i){
+		cout << i;
+		for (int j = 1; j < i; ++j){
+			cout << j;
+			cout << " ";
+		}
+		for (int k = n; k >= i; k--){
+			cout << k;
+			cout << "*";
+			
+		}
+		
+		cout << endl;
+	}
+
 
 	cin.get();
 	return 0;
