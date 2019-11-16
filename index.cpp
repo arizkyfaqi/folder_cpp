af49259300f10536c5a9dfd4ea45ce81278c2fac
Ade Rizky <aderizkyfaqi@gmail.com>
2019-11-16 19:41:38 +0700

    pola tiga sigitiga sikusiku

---
+++ index.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+	
+	//simplefy -> std::
+	using namespace std;
+	
+int main(){
+  cout << "Bismillahirohmanirohim" << endl;
+  cout << "Hallo Dunia !!!\n" << endl;
+  //Deklarasi Variable
+  int a;
+  //Inisialisasi varible
+  a = 10;
+  cout << "ini adalah nilai yg di inialisasi dari varible a : " << a << endl;
+
+  //Memasukan nilai dari konsol
+  int b;
+  cout << "masukan nilai : ";
+  cin >> b;
+  cout << "nilai yang anda masukan adalah : ";
+  cout << b << endl; 
+
+  cin.get();
+  return 0;
+}
--- l03_segitiga.cpp
+++ l03_segitiga.cpp
@@ -7,14 +7,38 @@ int main()
 	cout << "Masukan panjang pola: ";
 	cin >> n ;
 
-	cout << "Pola 1 \n";
+	// cout << "Pola 1 \n";
+	// for (int i = 1; i <= n; ++i){
+	// 	// cout << i;
+	// 	for (int j = 1; j <= i; ++j){
+	// 		// cout << j;
+	// 		cout << "*";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	// cout << "Pola 2 \n";
+	// for (int i = 1; i <= n; ++i){
+	// 	// cout << i;
+	// 	for (int j = n; j >= i; --j){
+	// 		// cout << j;
+	// 		cout << "*";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	cout << "Pola 3 \n";
 	for (int i = 1; i <= n; ++i){
-		cout << i;
-		for (int j = 1; j <= i; ++j){
-			cout << j;
+		for (int j = 1; j < i; ++j){
+			cout << " ";
+		}
+		for (int k = n; k >= i; --k)
+		{
 			cout << "*";
 		}
 		cout << endl;
 	}
+
+	cin.get();
 	return 0;
 }
