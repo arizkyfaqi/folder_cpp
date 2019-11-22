f787b31110a5dc5e40a7ea4af068920b15ca983c
Ade Rizky <aderizkyfaqi@gmail.com>
2019-11-22 08:15:11 +0700

    belajar fungsi

---
+++ 13_fungsi.cpp
@@ -0,0 +1,19 @@
+//include library
+#include <iostream>
+//include library math
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int x;
+
+	cout << "menghitung akar dari x: ";
+	cin >> x;
+
+	double y = sqrt(x);
+	cout << "akarnya adalah: "<< y << endl;
+
+	cin.get();
+	return 0;
+}
