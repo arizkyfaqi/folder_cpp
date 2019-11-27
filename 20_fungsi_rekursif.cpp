c1af65c8c4836d5130b882a75de686aea1dbdba3
Ade Rizky <aderizkyfaqi@gmail.com>
2019-11-27 08:48:16 +0700

    fungsi rekursif

---
+++ 20_fungsi_rekursif.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//fungsi rekursif terbatas
+int pangkatIterasi(int a, int b){
+	int hasil = a;
+	for (int i = 1; i < b; ++i)
+	{
+		hasil = hasil * a;
+	}
+	return hasil;
+}
+
+int pangkatRekursif(int a, int b){
+	if (b <= 1)
+	{
+		cout << "akhir dari rekursif\n";
+		return a;
+	} else {
+		cout << "rekursif\n";
+		return a * pangkatRekursif(a,(b - 1));
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int a, b;
+	cout << "angka: ";
+	cin >> a;
+	cout << "pangkat: ";
+	cin >> b;
+	cout << "hasil iterasi = " << pangkatIterasi(a,b) << endl;
+	cout << "hasil iterasi = " << pangkatRekursif(a,b) << endl;
+
+
+
+	cin.get();
+	return 0;
+}
