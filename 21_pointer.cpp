826899e23569eea205880cfc0b3dec554c860787
arfa <arfa@example.com>
2019-11-28 17:48:26 +0700

    belajar pointer

---
+++ 21_pointer.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int a = 5;
+
+/*
+
+	Belajar Pointer, 
+	Pointer adalah sebuah alamat variable dan nilai nya pada suatu memori
+
+*/
+
+//Pointer
+
+/*
+	
+	tanda * digunakan untuk menampung alamat varible;
+	tanda &(nama_varible) di gunakan untuk menampilkan alamat varible.
+
+*/
+
+int *aPtr = nullptr;
+	 aPtr = &a;
+
+int main(int argc, char const *argv[])
+{
+	cout << "nilai dari varible a : " << a << endl;
+	cout << "alamat dari varible a :" << aPtr << endl;
+
+	cin.get();
+	return 0;
+}
