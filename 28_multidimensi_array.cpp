387643924490e293d43f8a467f27cecb1cf53428
arfa <arfa@example.com>
2019-12-10 17:50:51 +0700

    array multidimensi

---
+++ 28_multidimensi_array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	//array multidimensi
+	// array [baris][kolom]
+	int array[2][3] = {1,2,3,4,5,6};
+
+	cout << array[0][0] << " " << array[0][1] << " " << array[1][0] << endl;
+
+	cin.get();
+	return 0;
+}
